d68bdae62db360086838280889851bef97b27c3a
Anselumus Galih Asmarandaru <angaliha26@Galih-MacBook-Pro.local>
2023-11-29 22:07:59 +0700

    feat: add tilemap basic

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,19 @@
+import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:ski_master/game/game.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const SkiMasterApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class SkiMasterApp extends StatelessWidget {
+  const SkiMasterApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Flutter Demo',
-      home: Text(''),
+      home: GameWidget.controlled(gameFactory: SkiMasterGame.new),
     );
   }
 }
