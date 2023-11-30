91d4cb09efcb7e78a45a025c2fc7a93cff9fb5c5
Galih A <galih.asmarandaru@gmail.com>
2023-11-30 09:39:59 +0700

    feat: add pause menu

--- lib/game/routes/gameplay.dart
+++ lib/game/routes/gameplay.dart
@@ -2,17 +2,31 @@ import 'dart:async';
 
 import 'package:flame/components.dart';
 import 'package:flame_tiled/flame_tiled.dart';
+import 'package:flutter/services.dart';
 
-class Gameplay extends Component {
-  Gameplay(this.currentLevel);
+class Gameplay extends Component with KeyboardHandler {
+  Gameplay(
+    this.currentLevel, {
+    super.key,
+    this.onPausePressed,
+  });
 
   static const id = 'Gameplay';
 
   final int currentLevel;
+  final VoidCallback? onPausePressed;
 
   @override
   Future<void> onLoad() async {
-    final map = await TiledComponent.load('sampleMap.tmx', Vector2.all(16));
+    final map = await TiledComponent.load('sampleMap.tmx', Vector2.all(64));
     await add(map);
+  }
+
+  @override
+  bool onKeyEvent(RawKeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
+    if (keysPressed.contains(LogicalKeyboardKey.keyP)) {
+      onPausePressed?.call();
+    }
+    return super.onKeyEvent(event, keysPressed);
   }
 }
